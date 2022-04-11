9245509dda23acf486baeef8d6ac73000d96335d
ArkinSolomon <arkin@arkinsolomon.net>
2022-04-11 14:35:47 -0400

    Add gate evaluations

--- CurcuitSimulator/Gate.cpp
+++ CurcuitSimulator/Gate.cpp
@@ -17,7 +17,8 @@ int Gate::getDelay() const
 
 Wire *Gate::getInput(int i) const
 {
-  if (i > 2 || i < 1) {
+  if (i > 2 || i < 1)
+  {
     return nullptr;
   }
   return (i == 1) ? input1 : input2;
@@ -30,5 +31,99 @@ Wire *Gate::getOutput() const
 
 char Gate::evaluate() const
 {
-  //
+  char in1 = input1->getValue();
+  switch (type)
+  {
+  case AND:
+    return eval_and();
+    break;
+
+  case OR:
+    return eval_or();
+    break;
+
+  case NOT:
+    return not_func(in1);
+    break;
+
+  case XOR:
+    return eval_xor();
+    break;
+
+  case NAND:
+    char andOut = eval_and();
+    return not_func(andOut);
+    break;
+
+  case NOR:
+    char orOut = eval_or();
+    return not_func(orOut);
+    break;
+
+  case XNOR:
+    char xorOut = eval_xor();
+    return not_func(xorOut);
+    break;
+
+  default:
+    return 'X';
+  }
+}
+
+friend char eval_and()
+{
+  char in2 = input2->getValue();
+  if (in1 == 'X' && in2 == 'X')
+  {
+    return 'X';
+  }
+  else if ((in1 == '1' && in2 == 'X') || (in1 == 'X' && in2 == '1'))
+  {
+    return 'X';
+  }
+  else if (in1 == '1' && in2 == '1')
+  {
+    return '1';
+  }
+  return '0';
+}
+
+friend char eval_or()
+{
+  char in2 = input2->getValue();
+  if (in1 == '1' || in2 == '1')
+  {
+    return '1';
+  }
+  else if (in1 == 'X' || in2 == 'X')
+  {
+    return 'X';
+  }
+  return '0';
+}
+
+friend char eval_xor()
+{
+  char in2 = input2->getValue();
+  if (in1 == in2)
+  {
+    return '0';
+  }
+  else
+  {
+    return '1';
+  }
+}
+
+char not_func(char in)
+{
+  if (in == '0')
+  {
+    return '1';
+  }
+  else if (in == '1')
+  {
+    return '0';
+  }
+  return 'X';
 }
