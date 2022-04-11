064012bb2a6ff271657eb7438bcec2e4c09a7238
ArkinSolomon <arkin@arkinsolomon.net>
2022-04-11 14:02:01 -0400

    Added gate.cpp

---
+++ CurcuitSimulator/Gate.cpp
@@ -0,0 +1,34 @@
+
+#include "Gate.h"
+
+Gate::Gate(GateType t, int d, Wire *in1, Wire *in2, Wire *out)
+{
+  type = t;
+  delay = d;
+  input1 = in1;
+  input2 = in2;
+  output = out;
+}
+
+int Gate::getDelay() const
+{
+  return delay;
+}
+
+Wire *Gate::getInput(int i) const
+{
+  if (i > 2 || i < 1) {
+    return nullptr;
+  }
+  return (i == 1) ? input1 : input2;
+}
+
+Wire *Gate::getOutput() const
+{
+  return output;
+}
+
+char Gate::evaluate() const
+{
+  //
+}
